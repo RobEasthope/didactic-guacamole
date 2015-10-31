844ce12512f3c5521e5b6c01fb634a0779ad2db0
Rob Easthope <robeasthope@gmail.com>
2015-10-31 23:23:03 +0000

    Style landing page and navigation

--- site/web/app/themes/sage/template-landing-page.php
+++ site/web/app/themes/sage/template-landing-page.php
@@ -5,6 +5,8 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <div class="title-wrap">
+    <h1 class="strapline"><?php the_field('strapline'); ?></h1>
+    <h4 class="tagline"><?php the_field('tagline'); ?></h4>
+  </div>
 <?php endwhile; ?>
