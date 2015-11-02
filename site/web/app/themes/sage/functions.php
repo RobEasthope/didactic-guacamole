7d270033b4f854167bf9f842eb03083391cdb696
Rob Easthope <robeasthope@gmail.com>
2015-11-02 08:26:45 +0000

    Add custom post types to taxonomy

--- site/web/app/themes/sage/functions.php
+++ site/web/app/themes/sage/functions.php
@@ -28,3 +28,16 @@ unset($file, $filepath);
 
 // Register Custom Navigation Walker (might be added via sage_includes?)
 require_once('lib/wp_bootstrap_navwalker.php');
+
+
+function add_custom_types_to_tax( $query ) {
+if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+// Get all your post types
+$post_types = get_post_types();
+
+$query->set( 'post_type', $post_types );
+return $query;
+}
+}
+add_filter( 'pre_get_posts', 'add_custom_types_to_tax' );
